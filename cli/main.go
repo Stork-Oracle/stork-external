8ce27c8885ec51c9023e88bc53a954fcb9df699b
Aaron Kawalsky <aaron@clued.xyz>
2024-07-19 12:35:17 -0400

    cleanup

--- cli/main.go
+++ cli/main.go
@@ -10,21 +10,32 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var verbose bool
+
 func main() {
-	zerolog.TimeFieldFormat = time.RFC3339Nano
-	zerolog.DurationFieldUnit = time.Nanosecond
-	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
-
-	// set global log level
-	zerolog.SetGlobalLevel(zerolog.DebugLevel)
-
 	rootCmd := &cobra.Command{
 		Use:   "storkcli",
 		Short: "Stork CLI tool for EVM contract updates based on WebSocket prices",
 		CompletionOptions: cobra.CompletionOptions{
 			HiddenDefaultCmd: true,
 		},
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			zerolog.TimeFieldFormat = time.RFC3339Nano
+			zerolog.DurationFieldUnit = time.Nanosecond
+			zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+			var logLevel zerolog.Level
+			if verbose {
+				logLevel = zerolog.DebugLevel
+			} else {
+				logLevel = zerolog.InfoLevel
+			}
+
+			// set global log level
+			zerolog.SetGlobalLevel(logLevel)
+		},
 	}
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose logging")
 
 	rootCmd.AddCommand(evmpushCmd)
 
