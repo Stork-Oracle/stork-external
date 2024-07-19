d4cc486c15f3bab7c249c432dcfe8bdfcd4a15dd
Aaron Kawalsky <aaron@clued.xyz>
2024-07-18 21:27:38 -0400

    add pusher cli

---
+++ cli/README.md
@@ -0,0 +1,3 @@
+# stork-external
+
+abigen --abi ../../../p_code/solidity-playground/stork.abi --pkg main --type StorkContract --out stork_contract.go
---
+++ cli/utilities.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"encoding/hex"
+	"os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func pluralize(n int) string {
+	if n == 1 {
+		return ""
+	}
+	return "s"
+}
+
+func stringToByte32(input string) ([32]byte, error) {
+	var result [32]byte
+
+	// Remove the "0x" prefix unconditionally
+	input = strings.TrimPrefix(input, "0x")
+
+	// Decode the hex string to bytes
+	bytes, err := hex.DecodeString(input)
+	if err != nil {
+		return result, err
+	}
+
+	// Check the length and copy to the result
+	if len(bytes) > 32 {
+		copy(result[:], bytes[len(bytes)-32:])
+	} else {
+		copy(result[32-len(bytes):], bytes)
+	}
+
+	return result, nil
+}
+
+func loadPrivateKey(mnemonicFile string) (*ecdsa.PrivateKey, error) {
+	// For simplicity, this example assumes the mnemonic file contains the private key directly
+	data, err := os.ReadFile(mnemonicFile)
+	if err != nil {
+		return nil, err
+	}
+	// Remove any trailing newline characters
+	dataString := strings.TrimSpace(string(data))
+
+	privateKey, err := crypto.HexToECDSA(dataString)
+	if err != nil {
+		return nil, err
+	}
+	return privateKey, nil
+}
