3994396b746fd23d8886a5eeadc5a1e42127432d
Harry Rackmil <harryrackmil@Harrys-MacBook-Pro.local>
2024-08-29 13:35:45 -0700

    use storknetwork org

--- scripts/push_to_docker.sh
+++ scripts/push_to_docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 TAG="v1.0.0"
-DOCKERHUB_USERNAME="harrystork"
+DOCKERHUB_USERNAME="storknetwork"
 IMAGE_NAME="stork-cli"
 docker build -f "$IMAGE_NAME".Dockerfile -t "$IMAGE_NAME":"$TAG" .
 docker tag "$IMAGE_NAME":"$TAG" "$DOCKERHUB_USERNAME"/"$IMAGE_NAME":"$TAG"
