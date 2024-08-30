eb8de02f077dec0644a18d06a636538186a2be6e
Harry Rackmil <harryrackmil@Harrys-MacBook-Pro.local>
2024-08-30 14:02:40 -0700

    remove --no-cache

--- scripts/push_to_docker.sh
+++ scripts/push_to_docker.sh
@@ -6,5 +6,5 @@ TAG="v1.0.0"
 DOCKERHUB_USERNAME="storknetwork"
 IMAGE_NAME="stork-cli"
 docker buildx use stork-cli-builder
-docker buildx build --platform linux/amd64,linux/arm64 -f "$IMAGE_NAME".Dockerfile -t "$DOCKERHUB_USERNAME"/"$IMAGE_NAME":"$TAG" --push --progress=plain --no-cache .
+docker buildx build --platform linux/amd64,linux/arm64 -f "$IMAGE_NAME".Dockerfile -t "$DOCKERHUB_USERNAME"/"$IMAGE_NAME":"$TAG" --push --progress=plain .
 echo "Pushed image successfully"
