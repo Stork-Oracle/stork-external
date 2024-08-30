5203ae16b7509885fbaba78fcd19f501bb8b191a
Harry Rackmil <harryrackmil@Harrys-MacBook-Pro.local>
2024-08-29 22:13:51 -0700

    build arm64 and amd64 docker images

--- scripts/push_to_docker.sh
+++ scripts/push_to_docker.sh
@@ -5,7 +5,6 @@ set -e
 TAG="v1.0.0"
 DOCKERHUB_USERNAME="storknetwork"
 IMAGE_NAME="stork-cli"
-docker build -f "$IMAGE_NAME".Dockerfile -t "$IMAGE_NAME":"$TAG" .
-docker tag "$IMAGE_NAME":"$TAG" "$DOCKERHUB_USERNAME"/"$IMAGE_NAME":"$TAG"
-docker push "$DOCKERHUB_USERNAME"/"$IMAGE_NAME":"$TAG"
+docker buildx use stork-cli-builder
+docker buildx build --platform linux/amd64,linux/arm64 -f "$IMAGE_NAME".Dockerfile -t "$DOCKERHUB_USERNAME"/"$IMAGE_NAME":"$TAG" --push .
 echo "Pushed image successfully"
