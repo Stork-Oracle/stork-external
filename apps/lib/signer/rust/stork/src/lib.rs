08a971dc1709102aaf7834e513555036a8b8e5fc
Harry Rackmil <harryrackmil@Harrys-MacBook-Pro.local>
2024-10-16 12:45:34 -0700

    add more tests, don't panic in rust

--- apps/lib/signer/rust/stork/src/lib.rs
+++ apps/lib/signer/rust/stork/src/lib.rs
@@ -32,19 +32,42 @@ pub extern "C" fn hash_and_sign(x_ptr: *const u8, y_ptr: *const u8, pk_ptr: *con
 
 #[no_mangle]
 pub extern "C" fn validate_stark_signature(x_ptr: *const u8, y_ptr: *const u8, public_key_ptr: *const u8, sig_r_ptr: *const u8, sig_s_ptr: *const u8) -> i32 {
-        let x_fe = bytes_to_field_element(x_ptr).unwrap_or_else(|_e| {panic!("Failed to convert x byte buffer to field element")});
-        let y_fe = bytes_to_field_element(y_ptr).unwrap_or_else(|_e| {panic!("Failed to convert y byte buffer to field element")});
-        let public_key_fe = bytes_to_field_element(public_key_ptr).unwrap_or_else(|_e| {panic!("Failed to convert public key byte buffer to field element")});
-        let sig_r_fe = bytes_to_field_element(sig_r_ptr).unwrap_or_else(|_e| {panic!("Failed to convert signature R byte buffer to field element")});
-        let sig_s_fe = bytes_to_field_element(sig_s_ptr).unwrap_or_else(|_e| {panic!("Failed to convert signature S byte buffer to field element")});
+    let x_fe = match bytes_to_field_element(x_ptr) {
+        Ok(val) => val,
+        Err(_) => return 0,
+    };
 
-        let hashed = pedersen_hash(&x_fe, &y_fe);
-        let signature = Signature{ r: sig_r_fe, s: sig_s_fe };
-        let result = starknet_core::crypto::ecdsa_verify(&public_key_fe, &hashed, &signature).unwrap_or_else(|_e| {panic!("Failed to verify pedersen hash")});
+    let y_fe = match bytes_to_field_element(y_ptr) {
+        Ok(val) => val,
+        Err(_) => return 0,
+    };
 
-        if result {
-                1
-        } else {
-                0
-        }
+    let public_key_fe = match bytes_to_field_element(public_key_ptr) {
+        Ok(val) => val,
+        Err(_) => return 0,
+    };
+
+    let sig_r_fe = match bytes_to_field_element(sig_r_ptr) {
+        Ok(val) => val,
+        Err(_) => return 0,
+    };
+
+    let sig_s_fe = match bytes_to_field_element(sig_s_ptr) {
+        Ok(val) => val,
+        Err(_) => return 0,
+    };
+
+    let hashed = pedersen_hash(&x_fe, &y_fe);
+    let signature = Signature { r: sig_r_fe, s: sig_s_fe };
+
+    let result = match starknet_core::crypto::ecdsa_verify(&public_key_fe, &hashed, &signature) {
+        Ok(val) => val,
+        Err(_) => return 0,
+    };
+
+    if result {
+        1
+    } else {
+        0
+    }
 }
--- apps/lib/signer/verifier_test.go
+++ apps/lib/signer/verifier_test.go
@@ -10,15 +10,62 @@ func TestEvmVerifier_VerifyPublisherPrice(t *testing.T) {
 		S: "0x79b4823a159988c04576ff71bc3ca168a631ac666094b0f4157e59b2892f6490",
 		V: "0x1b",
 	}
-	pubKey := PublisherKey("0x99e295e85cb07c16b7bb62a44df532a7f2620237")
 
-	err := VerifyEvmPublisherPrice(1710191092123456789, "BTCUSDMARK", "72147681412670819000000", pubKey, signature)
+	err := VerifyEvmPublisherPrice(
+		1710191092123456789,
+		"BTCUSDMARK",
+		"72147681412670819000000",
+		"0x99e295e85cb07c16b7bb62a44df532a7f2620237",
+		signature,
+	)
 	if err != nil {
 		t.Error(err)
 	}
 
-	// change the price slightly
-	err = VerifyEvmPublisherPrice(1710191092123456789, "BTCUSDMARK", "72147681412670719000000", pubKey, signature)
+	// changing the price causes the signature to be invalid
+	err = VerifyEvmPublisherPrice(
+		1710191092123456789,
+		"BTCUSDMARK",
+		"72147681412670719000000",
+		"0x99e295e85cb07c16b7bb62a44df532a7f2620237",
+		signature,
+	)
+	if err == nil {
+		t.Fail()
+	}
+
+	// changing the asset id causes the signature to be invalid
+	err = VerifyEvmPublisherPrice(
+		1710191092123456789,
+		"BTCUSDMARK2",
+		"72147681412670819000000",
+		"0x99e295e85cb07c16b7bb62a44df532a7f2620237",
+		signature,
+	)
+	if err == nil {
+		t.Fail()
+	}
+
+	// changing the timestamp causes the signature to be invalid
+	err = VerifyEvmPublisherPrice(
+		1710191192123456789,
+		"BTCUSDMARK",
+		"72147681412670819000000",
+		"0x99e295e85cb07c16b7bb62a44df532a7f2620237",
+		signature,
+	)
+	if err == nil {
+		t.Fail()
+	}
+
+	// changing the publisher key causes the signature to be invalid
+	err = VerifyEvmPublisherPrice(
+		1710191092123456789,
+		"BTCUSDMARK",
+		"72147681412670819000000",
+		"0x98e295e85cb07c16b7bb62a44df532a7f2620237",
+		signature,
+	)
 	if err == nil {
 		t.Fail()
 	}
@@ -29,18 +76,66 @@ func TestStarkVerifier_VerifyPublisherPrice(t *testing.T) {
 		R: "0x60bbbb4142bca69a5278ecccb59964e3449e43915b02e5c729b9752a16309ac",
 		S: "0x4cdbe54b985f6fb4495398f94554883ead7cbb983597dc7ea8b9e32dfe95c27",
 	}
-	pubKey := PublisherKey("0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06")
 
-	err := VerifyStarkPublisherPrice(1708940577123456789, "0x44594458555344000000000000000000637a6f7778", "3335950349880000000", pubKey, signature)
+	err := VerifyStarkPublisherPrice(
+		1708940577123456789,
+		"0x44594458555344000000000000000000637a6f7778",
+		"3335950349880000000",
+		"0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06",
+		signature,
+	)
 	if err != nil {
 		t.Error(err)
 	}
 
-	// change the price slightly
-	err = VerifyStarkPublisherPrice(1708940577123456789, "0x44594458555344000000000000000000637a6f7778", "3335950348880000000", pubKey, signature)
+	// changing the price causes the signature to be invalid
+	err = VerifyStarkPublisherPrice(
+		1708940577123456789,
+		"0x44594458555344000000000000000000637a6f7778",
+		"3335950348880000000",
+		"0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06",
+		signature,
+	)
 	if err == nil {
 		t.Fail()
 	}
+
+	// changing the external asset id causes the signature to be invalid
+	err = VerifyStarkPublisherPrice(
+		1708940577123456789,
+		"0x44594458555344000001000000000000637a6f7778",
+		"3335950349880000000",
+		"0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06",
+		signature,
+	)
+	if err == nil {
+		t.Fail()
+	}
+
+	// changing the timestamp causes the signature to be invalid
+	err = VerifyStarkPublisherPrice(
+		1708940576123456789,
+		"0x44594458555344000000000000000000637a6f7778",
+		"3335950349880000000",
+		"0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06",
+		signature,
+	)
+	if err == nil {
+		t.Fail()
+	}
+
+	// changing the publisher key causes the signature to be invalid
+	err = VerifyStarkPublisherPrice(
+		1708940577123456789,
+		"0x44594458555344000000000000000000637a6f7778",
+		"3335950349880000000",
+		"0x419d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06",
+		signature,
+	)
+	if err == nil {
+		t.Fail()
+	}
+
 }
 
 func TestStarkVerifier_VerifyPublisherPriceLongAssetId(t *testing.T) {
@@ -50,7 +145,13 @@ func TestStarkVerifier_VerifyPublisherPriceLongAssetId(t *testing.T) {
 	}
 	pubKey := PublisherKey("0x2798bbe74d340f938e8151b4af9992481dbb952ed359e2c46cf23021d6befd8")
 
-	err := VerifyStarkPublisherPrice(1729023715673877869, "444a5457494e594553555344545741503438307073727631", "574709288691000000", pubKey, signature)
+	err := VerifyStarkPublisherPrice(
+		1729023715673877869,
+		"444a5457494e594553555344545741503438307073727631",
+		"574709288691000000",
+		pubKey,
+		signature,
+	)
 	if err != nil {
 		t.Error(err)
 	}
