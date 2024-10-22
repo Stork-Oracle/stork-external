be2b66bc5d2ae8a261d63a731317a7556886f922
Henry Baldwin <henry@stork.network>
2024-10-22 17:21:13 -0400

    Updated Keys test to more thouroughly test key loading, including from file, and ensuring overwrite from env.

--- apps/lib/publisher_agent/config_test.go
+++ apps/lib/publisher_agent/config_test.go
@@ -1,6 +1,7 @@
 package publisher_agent
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
@@ -23,22 +24,41 @@ func TestLoadConfig(t *testing.T) {
 	assert.Equal(t, 5216, config.IncomingWsPort)
 }
 
-func TestLoadKeysFileFromEnv(t *testing.T) {
-	evmPrivateKey := "0x8b558d5fc31eb64bb51d44b4b28658180e96764d5d5ac68e6d124f86f576d9de"
-	evmPublicKey := "0x99e295e85cb07c16b7bb62a44df532a7f2620237"
-	starkPrivateKey := "0x66253bdeb3c1a235cf4376611e3a14474e2c00fd2fb225f9a388faae7fb095a"
-	starkPublicKey := "0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06"
+func TestLoadKeys(t *testing.T) {
+	// load keys from file
+	keysFileData, err := readFile("./resources/example_keys.json")
+	assert.NoError(t, err)
+	assert.NotNil(t, keysFileData)
 
-	os.Setenv("STORK_EVM_PRIVATE_KEY", evmPrivateKey)
-	os.Setenv("STORK_EVM_PUBLIC_KEY", evmPublicKey)
-	os.Setenv("STORK_STARK_PRIVATE_KEY", starkPrivateKey)
-	os.Setenv("STORK_STARK_PUBLIC_KEY", starkPublicKey)
+	var keys Keys
+	err = json.Unmarshal(keysFileData, &keys)
+	assert.NoError(t, err)
+	assert.Equal(t, signer.EvmPrivateKey("0x8b558d5fc31eb64bb51d44b4b28658180e96764d5d5ac68e6d124f86f576d9de"), keys.EvmPrivateKey)
+	assert.Equal(t, signer.EvmPublisherKey("0x99e295e85cb07c16b7bb62a44df532a7f2620237"), keys.EvmPublicKey)
+	assert.Equal(t, signer.StarkPrivateKey("0x66253bdeb3c1a235cf4376611e3a14474e2c00fd2fb225f9a388faae7fb095a"), keys.StarkPrivateKey)
+	assert.Equal(t, signer.StarkPublisherKey("0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a06"), keys.StarkPublicKey)
 
-	var keysFile Keys
-	loadKeysFromEnv(&keysFile)
+	envEvmPrivateKey := "0x8b558d5fc31eb64bb51d44b4b28658180e96764d5d5ac68e6d124f86f576d9d4"
+	envEvmPublicKey := "0x99e295e85cb07c16b7bb62a44df532a7f2620234"
+	envStarkPrivateKey := "0x66253bdeb3c1a235cf4376611e3a14474e2c00fd2fb225f9a388faae7fb0954"
+	envStarkPublicKey := "0x418d3fd8219a2cf32a00d458f61802d17f01c5bcde5a4f82008ee4a7c8e9a04"
 
-	assert.Equal(t, signer.EvmPrivateKey(evmPrivateKey), keysFile.EvmPrivateKey)
-	assert.Equal(t, signer.EvmPublisherKey(evmPublicKey), keysFile.EvmPublicKey)
-	assert.Equal(t, signer.StarkPrivateKey(starkPrivateKey), keysFile.StarkPrivateKey)
-	assert.Equal(t, signer.StarkPublisherKey(starkPublicKey), keysFile.StarkPublicKey)
+	os.Setenv("STORK_EVM_PRIVATE_KEY", envEvmPrivateKey)
+	os.Setenv("STORK_EVM_PUBLIC_KEY", envEvmPublicKey)
+	os.Setenv("STORK_STARK_PRIVATE_KEY", envStarkPrivateKey)
+	os.Setenv("STORK_STARK_PUBLIC_KEY", envStarkPublicKey)
+
+	// ensure env keys aren't the same as file keys
+	assert.NotEqual(t, keys.EvmPrivateKey, signer.EvmPrivateKey(envEvmPrivateKey))
+	assert.NotEqual(t, keys.EvmPublicKey, signer.EvmPrivateKey(envEvmPublicKey))
+	assert.NotEqual(t, keys.StarkPrivateKey, signer.EvmPrivateKey(envStarkPrivateKey))
+	assert.NotEqual(t, keys.StarkPublicKey, signer.EvmPrivateKey(envStarkPublicKey))
+
+	keys.updateFromEnvVars()
+
+	// ensure all keys were overwritten
+	assert.Equal(t, signer.EvmPrivateKey(envEvmPrivateKey), keys.EvmPrivateKey)
+	assert.Equal(t, signer.EvmPublisherKey(envEvmPublicKey), keys.EvmPublicKey)
+	assert.Equal(t, signer.StarkPrivateKey(envStarkPrivateKey), keys.StarkPrivateKey)
+	assert.Equal(t, signer.StarkPublisherKey(envStarkPublicKey), keys.StarkPublicKey)
 }
