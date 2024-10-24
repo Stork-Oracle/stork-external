52e1a322847148ef4f5107abae50c8596c4d82ed
Aaron Kawalsky <aaron@stork.network>
2024-10-24 11:54:03 -0400

    set up admin script

--- contracts/solana/stork/app/stork-client.ts
+++ contracts/solana/stork/app/stork-client.ts
@@ -1,144 +1,45 @@
+import { Argument, Command } from "commander";
+
 import * as anchor from "@coral-xyz/anchor";
-import { SystemProgram, PublicKey } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 import { BN } from "bn.js";
 import { Stork } from "../target/types/stork";
 
+const anchorProviderUrl = process.env.ANCHOR_PROVIDER_URL;
+const anchorWallet = process.env.ANCHOR_WALLET;
+
 // Constants
-const STORK_PROGRAM_ID = new PublicKey(
-  "F8C2ffLfHGQTQxnix4LaroXuHh2Cdv8wJw7R9UDEfmX1"
-);
 const STORK_FEED_SEED = Buffer.from("stork_feed");
 const STORK_CONFIG_SEED = Buffer.from("stork_config");
 const STORK_TREASURY_SEED = Buffer.from("stork_treasury");
 
-// Function to write a new value to the feed
-async function writeToFeed(
-  program: anchor.Program<Stork>,
-  payer: anchor.web3.Keypair,
-  updateData: any,
-  treasuryId: number
-) {
-  // Derive the PDA for the feed and the treasury account
-  const [treasuryPDA] = await PublicKey.findProgramAddressSync(
-    [STORK_TREASURY_SEED, new Uint8Array([treasuryId])],
-    program.programId
-  );
-
-  // Call the `update_temporal_numeric_value_evm` instruction
-
-  await program.methods
-    .updateTemporalNumericValueEvm(updateData)
-    .accounts({
-      treasury: treasuryPDA,
-      payer: payer.publicKey,
-    })
-    .signers([payer])
-    .rpc()
-    .catch((err) => {
-      console.error("Error:", err);
-    });
-
-  console.log("Feed updated successfully!");
-}
-
-// Function to retrieve the feed data
-async function getFeed(program: anchor.Program<Stork>, feedId: any) {
-  const [feedPDA] = await PublicKey.findProgramAddressSync(
-    [STORK_FEED_SEED, Buffer.from(feedId)],
-    program.programId
-  );
-
-  const feedAccount = await program.account.temporalNumericValueFeed.fetch(
-    feedPDA
-  );
-  console.log("Feed Data:", feedAccount);
-  return feedAccount;
-}
-
-// Function to get the config PDA
-async function getConfigPda(program: anchor.Program<Stork>) {
-  const [configPDA] = await PublicKey.findProgramAddressSync(
-    [STORK_CONFIG_SEED],
-    program.programId
-  );
-  return configPDA;
+if (!anchorProviderUrl || !anchorWallet) {
+  throw new Error("ANCHOR_PROVIDER_URL and ANCHOR_WALLET must be set");
 }
 
 function hexStringToByteArray(hexString) {
+  if (hexString.startsWith("0x")) {
+    hexString = hexString.slice(2);
+  }
   return Array.from(Buffer.from(hexString, "hex"));
 }
 
-// Add this new function to initialize the Stork program
-async function initializeStorkProgram(
-  program: anchor.Program<Stork>,
-  payer: anchor.web3.Keypair
-) {
-  const [configPDA] = await PublicKey.findProgramAddressSync(
-    [STORK_CONFIG_SEED],
-    program.programId
-  );
-
-  // You'll need to replace these with actual values
-  const storkEvmPublicKey = hexStringToByteArray("0a803F9b1CCe32e2773e0d2e98b37E0775cA5d44");
-  const singleUpdateFeeInLamports = new BN(1);
-
-  console.log("Config PDA:", configPDA.toBase58());
-  console.log("Payer public key:", payer.publicKey.toBase58());
-  console.log("Program ID:", program.programId.toBase58());
-
-  try {
-    const tx = await program.methods
-      .initialize(payer.publicKey, storkEvmPublicKey, singleUpdateFeeInLamports)
-      .accounts({
-        owner: payer.publicKey,
-      })
-      .remainingAccounts([
-        {
-          pubkey: anchor.web3.SystemProgram.programId,
-          isWritable: false,
-          isSigner: false,
-        },
-      ])
-      .signers([payer])
-      .transaction();
-
-    tx.feePayer = payer.publicKey;
-
-    console.log("Transaction created, simulating...");
-    const simulation = await program.provider.connection.simulateTransaction(tx);
-    console.log("Simulation result:", simulation.value);
-
-    if (simulation.value.err) {
-      console.error("Simulation failed:", simulation.value.err);
-      return;
-    }
-
-    console.log("Simulation successful, sending transaction...");
-    const txid = await program.provider.sendAndConfirm(tx, [payer]);
-    console.log("Transaction sent:", txid);
-
-    console.log("Stork program initialized successfully!");
-  } catch (error) {
-    if (error.message.includes("already in use")) {
-      console.log("Stork program already initialized.");
-    } else {
-      console.error("Error initializing Stork program:", error);
-      if (error instanceof anchor.web3.SendTransactionError) {
-        console.error("Transaction logs:", error.logs);
-      }
-      throw error;
-    }
-  }
+function hexArrayToString(hexArray: number[]) {
+  return Buffer.from(hexArray).toString("hex");
 }
 
-// Main function to run the script
-(async () => {
+const initializeCliProgram = (): {
+  program: anchor.Program<Stork>;
+  payer: anchor.web3.Keypair;
+} => {
   console.log("Initializing provider...");
   const provider = anchor.AnchorProvider.env();
-  const fs = require('fs');
+  const fs = require("fs");
 
   const payerKeypair = anchor.web3.Keypair.fromSecretKey(
-    Uint8Array.from(JSON.parse(fs.readFileSync(process.env.ANCHOR_WALLET!, 'utf-8')))
+    Uint8Array.from(
+      JSON.parse(fs.readFileSync(process.env.ANCHOR_WALLET!, "utf-8"))
+    )
   );
 
   anchor.setProvider(provider);
@@ -149,41 +50,279 @@ async function initializeStorkProgram(
   console.log("Program ID:", idl.address);
 
   console.log("Initializing program...");
-  const program = new anchor.Program<Stork>(idl, provider);
-
-  // Add this line to initialize the Stork program
-  await initializeStorkProgram(program, payerKeypair);
-
-  const treasuryId = 1;
-  const feedId = hexStringToByteArray(
-    "7404e3d104ea7841c3d9e6fd20adfe99b4ad586bc08d8f3bd3afef894cf184de"
-  );
-  const updateData = {
-    temporalNumericValue: {
-      timestampNs: new anchor.BN("1722632569208762117"),
-      quantizedValue: new anchor.BN("62507457175499998000000"),
-    },
-    id: feedId,
-    publisherMerkleRoot: hexStringToByteArray(
-      "e5ff773b0316059c04aa157898766731017610dcbeede7d7f169bfeaab7cc318"
-    ),
-    valueComputeAlgHash: hexStringToByteArray(
-      "9be7e9f9ed459417d96112a7467bd0b27575a2c7847195c68f805b70ce1795ba"
-    ),
-    r: hexStringToByteArray(
-      "b9b3c9f80a355bd0cd6f609fff4a4b15fa4e3b4632adabb74c020f5bcd240741"
-    ),
-    s: hexStringToByteArray(
-      "16fab526529ac795108d201832cff8c2d2b1c710da6711fe9f7ab288a7149758"
-    ),
-    v: 28,
-    treasuryId,
+  return {
+    program: new anchor.Program<Stork>(idl, provider),
+    payer: payerKeypair,
   };
-  // Write to the feed
+};
 
-  await writeToFeed(program, payerKeypair, updateData, treasuryId);
+const cliProgram = new Command();
+cliProgram
+  .name("sol-stork")
+  .description("Solana Stork client")
+  .version("0.1.0");
 
-  // Retrieve the feed data
-  const feedData = await getFeed(program, feedId);
-  console.log("Retrieved feed data:", feedData);
-})();
+const DEFAULT_SINGLE_UPDATE_FEE_IN_LAMPORTS = 1;
+const DEFAULT_STORK_SOLANA_ADDRESS =
+  "9yjwoWUgyKeH2cEC4S5G9uudobYAcmDH9zU1mq1hKWyb";
+const DEFAULT_STORK_EVM_ADDRESS = "0x0a803F9b1CCe32e2773e0d2e98b37E0775cA5d44";
+
+cliProgram
+  .command("initialize")
+  .description("Initialize the Stork program")
+  .argument(
+    "[stork_solana_address]",
+    "The Stork Solana address",
+    (value) => value,
+    DEFAULT_STORK_SOLANA_ADDRESS
+  )
+  .argument(
+    "[stork_evm_address]",
+    "The Stork EVM address",
+    (value) => value,
+    DEFAULT_STORK_EVM_ADDRESS
+  )
+  .argument(
+    "[single_update_fee_in_lamports]",
+    "The single update fee in lamports",
+    (value) => parseInt(value),
+    DEFAULT_SINGLE_UPDATE_FEE_IN_LAMPORTS
+  )
+  .action(
+    async (
+      storkSolanaAddress,
+      storkEvmPublicKey,
+      singleUpdateFeeInLamports
+    ) => {
+      const { program, payer } = initializeCliProgram();
+      try {
+        await program.methods
+          .initialize(
+            new PublicKey(storkSolanaAddress),
+            hexStringToByteArray(storkEvmPublicKey),
+            new BN(singleUpdateFeeInLamports)
+          )
+          .accounts({
+            owner: payer.publicKey,
+          })
+          .remainingAccounts([
+            {
+              pubkey: anchor.web3.SystemProgram.programId,
+              isWritable: false,
+              isSigner: false,
+            },
+          ])
+          .signers([payer])
+          .rpc();
+
+        console.log("Stork program initialized successfully!");
+      } catch (error) {
+        if (error.message.includes("already in use")) {
+          console.log("Stork program already initialized.");
+        } else {
+          console.error("Error initializing Stork program:", error);
+          if (error instanceof anchor.web3.SendTransactionError) {
+            console.error("Transaction logs:", error.logs);
+          }
+          throw error;
+        }
+      }
+    }
+  );
+
+cliProgram
+  .command("stork-evm-public-key")
+  .description("Get the EVM address")
+  .action(async () => {
+    const { program } = initializeCliProgram();
+
+    const [configPda] = await PublicKey.findProgramAddressSync(
+      [STORK_CONFIG_SEED],
+      program.programId
+    );
+
+    const config = await program.account.storkConfig.fetch(configPda);
+    console.log(hexArrayToString(config.storkEvmPublicKey));
+  });
+
+cliProgram
+  .command("stork-solana-public-key")
+  .description("Get the Solana address")
+  .action(async () => {
+    const { program } = initializeCliProgram();
+
+    const [configPda] = await PublicKey.findProgramAddressSync(
+      [STORK_CONFIG_SEED],
+      program.programId
+    );
+
+    const config = await program.account.storkConfig.fetch(configPda);
+    console.log(config.storkSolPublicKey.toBase58());
+  });
+
+cliProgram
+  .command("single-update-fee")
+  .description("Get the single update fee")
+  .action(async () => {
+    const { program } = initializeCliProgram();
+
+    const [configPda] = await PublicKey.findProgramAddressSync(
+      [STORK_CONFIG_SEED],
+      program.programId
+    );
+    const config = await program.account.storkConfig.fetch(configPda);
+    console.log(config.singleUpdateFeeInLamports.toString());
+  });
+
+cliProgram
+  .command("update-stork-evm-public-key")
+  .description("Update the EVM public key")
+  .argument("evm_public_key", "The EVM public key")
+  .action(async (evmPublicKey) => {
+    const { program } = initializeCliProgram();
+    const configPda = await PublicKey.findProgramAddressSync(
+      [STORK_CONFIG_SEED],
+      program.programId
+    );
+    await program.methods.updateStorkEvmPublicKey(hexStringToByteArray(evmPublicKey)).accounts({
+      config: configPda,
+    }).rpc();
+  });
+
+cliProgram
+  .command("update-stork-solana-public-key")
+  .description("Update the Solana public key")
+  .argument("solana_public_key", "The Solana public key")
+  .action(async (solanaPubKey) => {
+    const { program } = initializeCliProgram();
+    const [configPda] = await PublicKey.findProgramAddressSync(
+      [STORK_CONFIG_SEED],
+      program.programId
+    );
+    await program.methods.updateStorkSolPublicKey(new PublicKey(solanaPubKey)).accounts({
+      config: configPda,
+    }).rpc();
+    console.log("Solana public key updated successfully.");
+  });
+
+cliProgram
+  .command("update-single-update-fee")
+  .description("Update the single update fee")
+  .argument("fee", "The new single update fee in lamports")
+  .action(async (fee) => {
+    const { program } = initializeCliProgram();
+    const [configPda] = await PublicKey.findProgramAddressSync(
+      [STORK_CONFIG_SEED],
+      program.programId
+    );
+    await program.methods.updateSingleUpdateFeeInLamports(new BN(fee)).accounts({
+      config: configPda,
+    }).rpc();
+    console.log("Single update fee updated successfully.");
+  });
+
+cliProgram
+  .command("write-to-feed")
+  .description("Write to a feed")
+  .argument("asset_pair", "The asset pair")
+  .argument("endpoint", "The REST endpoint")
+  .argument("auth_key", "The auth key")
+  .action(async (assetPair, restUrl, authKey) => {
+    console.log("Writing to feed...");
+    const { program, payer } = initializeCliProgram();
+    try {
+      const result = await fetch(
+        `${restUrl}/v1/prices/latest\?assets\=${assetPair}`,
+        {
+          headers: {
+            Authorization: `Basic ${authKey}`,
+          },
+        }
+      );
+      const rawJson = await result.text();
+      const safeJsonText = rawJson.replace(
+        /(?<!["\d])\b\d{16,}\b(?!["])/g, // Regex to find large integers not already in quotes
+        (match) => `"${match}"` // Convert large numbers to strings
+      );
+
+      const responseData = JSON.parse(safeJsonText);
+      console.log(
+        responseData.data[assetPair].stork_signed_price.timestamped_signature
+          .signature.r
+      );
+
+      const treasuryId = Math.floor(Math.random() * 256);
+      console.log(`Generated random treasury ID: ${treasuryId}`);
+      const updateData = {
+        temporalNumericValue: {
+          timestampNs: new anchor.BN(
+            responseData.data[
+              assetPair
+            ].stork_signed_price.timestamped_signature.timestamp
+          ),
+          quantizedValue: new anchor.BN(
+            responseData.data[assetPair].stork_signed_price.price
+          ),
+        },
+        id: hexStringToByteArray(
+          responseData.data[assetPair].stork_signed_price.encoded_asset_id
+        ),
+        publisherMerkleRoot: hexStringToByteArray(
+          responseData.data[assetPair].stork_signed_price.publisher_merkle_root
+        ),
+        valueComputeAlgHash: hexStringToByteArray(
+          responseData.data[assetPair].stork_signed_price.calculation_alg
+            .checksum
+        ),
+        r: hexStringToByteArray(
+          responseData.data[assetPair].stork_signed_price.timestamped_signature
+            .signature.r
+        ),
+        s: hexStringToByteArray(
+          responseData.data[assetPair].stork_signed_price.timestamped_signature
+            .signature.s
+        ),
+        v: hexStringToByteArray(
+          responseData.data[assetPair].stork_signed_price.timestamped_signature
+            .signature.v
+        )[0],
+        treasuryId,
+      };
+
+      // Derive the PDA for the feed and the treasury account
+      const [treasuryPDA] = await PublicKey.findProgramAddressSync(
+        [STORK_TREASURY_SEED, new Uint8Array([treasuryId])],
+        program.programId
+      );
+
+      await program.methods
+        .updateTemporalNumericValueEvm(updateData)
+        .accounts({
+          treasury: treasuryPDA,
+          payer: payer.publicKey,
+        })
+        .signers([payer])
+        .rpc();
+
+      console.log("Feed updated successfully!");
+    } catch (error) {
+      console.error("Error writing to feed:", error);
+    }
+  });
+
+cliProgram
+  .command("get-feed")
+  .description("Get a feed")
+  .argument("feed_id", "The feed ID")
+  .action(async (feedId) => {
+    const { program } = initializeCliProgram();
+    const [feedPda] = await PublicKey.findProgramAddressSync(
+      [STORK_FEED_SEED, Buffer.from(hexStringToByteArray(feedId))],
+      program.programId
+    );
+    const feed = await program.account.temporalNumericValueFeed.fetch(feedPda);
+    console.log(feed.latestValue.quantizedValue.toString());
+    console.log(feed.latestValue.timestampNs.toString());
+  });
+
+cliProgram.parse();
