140eb776735e1418cd6003c678d9fbc454ab4605
Henry Baldwin <henry@stork.network>
2024-10-25 16:10:10 -0400

    First pass setting up command, interacter, and pusher for solana.

--- apps/lib/chain_pusher/solana-push.go
+++ apps/lib/chain_pusher/solana-push.go
@@ -16,8 +16,7 @@ func init() {
 	SolanapushCmd.Flags().StringP(ChainRpcUrlFlag, "c", "", ChainRpcUrlDesc)
 	SolanapushCmd.Flags().StringP(ContractAddressFlag, "x", "", ContractAddressDesc)
 	SolanapushCmd.Flags().StringP(AssetConfigFileFlag, "f", "", AssetConfigFileDesc)
-	SolanapushCmd.Flags().StringP(MnemonicFileFlag, "m", "", MnemonicFileDesc)
-	SolanapushCmd.Flags().BoolP(VerifyPublishersFlag, "v", false, VerifyPublishersDesc)
+	SolanapushCmd.Flags().StringP(PrivateKeyFileFlag, "k", "", PrivateKeyFileDesc)
 	SolanapushCmd.Flags().IntP(BatchingWindowFlag, "b", 5, BatchingWindowDesc)
 	SolanapushCmd.Flags().IntP(PollingFrequencyFlag, "p", 3, PollingFrequencyDesc)
 
@@ -30,20 +29,21 @@ func init() {
 }
 
 func runSolanaPush(cmd *cobra.Command, args []string) {
-	// storkWsEndpoint, _ := cmd.Flags().GetString(StorkWebsocketEndpointFlag)
-	// storkAuth, _ := cmd.Flags().GetString(StorkAuthCredentialsFlag)
-	// chainRpcUrl, _ := cmd.Flags().GetString(ChainRpcUrlFlag)
-	// contractAddress, _ := cmd.Flags().GetString(ContractAddressFlag)
-	// assetConfigFile, _ := cmd.Flags().GetString(AssetConfigFileFlag)
-	// mnemonicFile, _ := cmd.Flags().GetString(MnemonicFileFlag)
-	// verifyPublishers, _ := cmd.Flags().GetBool(VerifyPublishersFlag)
-	// batchingWindow, _ := cmd.Flags().GetInt(BatchingWindowFlag)
-	// pollingFrequency, _ := cmd.Flags().GetInt(PollingFrequencyFlag)
+	storkWsEndpoint, _ := cmd.Flags().GetString(StorkWebsocketEndpointFlag)
+	storkAuth, _ := cmd.Flags().GetString(StorkAuthCredentialsFlag)
+	chainRpcUrl, _ := cmd.Flags().GetString(ChainRpcUrlFlag)
+	contractAddress, _ := cmd.Flags().GetString(ContractAddressFlag)
+	assetConfigFile, _ := cmd.Flags().GetString(AssetConfigFileFlag)
+	privateKeyFile, _ := cmd.Flags().GetString(PrivateKeyFileFlag)
+	batchingWindow, _ := cmd.Flags().GetInt(BatchingWindowFlag)
+	pollingFrequency, _ := cmd.Flags().GetInt(PollingFrequencyFlag)
 
-	// logger := SolanaPusherLogger(chainRpcUrl, contractAddress)
+	logger := SolanaPusherLogger(chainRpcUrl, contractAddress)
 
-	// TODO - add solanaInteracter
-
-	// evmPusher := NewPusher(storkWsEndpoint, storkAuth, chainRpcUrl, contractAddress, assetConfigFile, mnemonicFile, verifyPublishers, batchingWindow, pollingFrequency, solanaInteracter, &logger)
-	// evmPusher.Run()
+	solanaInteracter, err := NewSolanaContractInteracter(chainRpcUrl, storkWsEndpoint, contractAddress, privateKeyFile, assetConfigFile, pollingFrequency, logger)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to initialize Solana contract interacter")
+	}
+	solanaPusher := NewPusher(storkWsEndpoint, storkAuth, chainRpcUrl, contractAddress, assetConfigFile, batchingWindow, pollingFrequency, solanaInteracter, &logger)
+	solanaPusher.Run()
 }
