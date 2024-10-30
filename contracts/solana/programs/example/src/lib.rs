3d28c9c42a366107f50b4cee68f444268f61f514
Aaron Kawalsky <aaron@stork.network>
2024-10-30 17:04:31 -0400

    improve example

--- contracts/solana/programs/example/src/lib.rs
+++ contracts/solana/programs/example/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use stork_sdk::{
     pda::STORK_FEED_SEED,
-    temporal_numeric_value::TemporalNumericValueFeed,
+    temporal_numeric_value::{FeedId, TemporalNumericValueFeed},
 };
 
 declare_id!("FGpoDwQC8gYadJAsB9vrsgPN38qkqDgSk3qQcaRiyPra");
@@ -11,12 +11,14 @@ pub mod example {
     use super::*;
 
     // This instruction reads the latest price from a Stork feed
-    pub fn read_price(ctx: Context<ReadPrice>, feed_id: [u8; 32]) -> Result<()> {
+    pub fn read_price(ctx: Context<ReadPrice>, feed_id: FeedId) -> Result<()> {
         let feed = &ctx.accounts.feed;
+
+        let latest_value = feed.get_latest_canonical_temporal_numeric_value_unchecked(&feed_id)?;
         
         // Get the latest timestamp and value from the feed
-        let timestamp = feed.latest_value.timestamp_ns;
-        let value = feed.latest_value.quantized_value;
+        let timestamp = latest_value.timestamp_ns;
+        let value = latest_value.quantized_value;
         
         msg!(
             "Feed {} - Latest value: {}, Timestamp: {}", 
@@ -32,7 +34,7 @@ pub mod example {
 }
 
 #[derive(Accounts)]
-#[instruction(feed_id: [u8; 32])]
+#[instruction(feed_id: FeedId)]
 pub struct ReadPrice<'info> {
     // This account holds the price feed data
     #[account(
