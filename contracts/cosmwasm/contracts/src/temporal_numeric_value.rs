e9a39b2b1e41e2a5b8f192fd1814590aee5e7b27
Henry Baldwin <henry@stork.network>
2025-01-24 13:22:31 -0500

    Change timestamp in temporal numeric value to be a custom CosmWasm Uint64 for serialization purposes.

--- contracts/cosmwasm/contracts/src/temporal_numeric_value.rs
+++ contracts/cosmwasm/contracts/src/temporal_numeric_value.rs
@@ -1,17 +1,17 @@
-use sylvia::{cw_schema::cw_serde, cw_std::Int128};
+use sylvia::{cw_schema::cw_serde, cw_std::{Int128, Uint64}};
 
 pub type EncodedAssetId = [u8; 32];
 
 #[cw_serde(crate = "sylvia")]
 pub struct TemporalNumericValue {
-    pub timestamp_ns: u64,
+    pub timestamp_ns: Uint64,
     pub quantized_value: Int128,
 }
 
 impl TemporalNumericValue {
     pub fn new(timestamp_ns: u64, quantized_value: i128) -> Self {
         TemporalNumericValue {
-            timestamp_ns,
+            timestamp_ns: timestamp_ns.into(),
             quantized_value: quantized_value.into(),
         }
     }
