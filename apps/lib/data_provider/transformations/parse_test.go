cb86028ea4439b066c01d238ed2dce76f15ad9f2
akawalsky <aaron@stork.network>
2025-02-04 13:49:50 -0500

    add more unit tests

--- apps/lib/data_provider/transformations/parse_test.go
+++ apps/lib/data_provider/transformations/parse_test.go
@@ -91,3 +91,127 @@ func TestParse(t *testing.T) {
 		})
 	}
 }
+
+func TestGetDependencies(t *testing.T) {
+	tests := []struct {
+		name     string
+		formula  string
+		expected []string
+	}{
+		{
+			name:     "simple addition",
+			formula:  "1 + 2",
+			expected: []string{},
+		},
+		{
+			name:     "simple addition with variables",
+			formula:  "x + y",
+			expected: []string{"x", "y"},
+		},
+		{
+			name:     "nested expression",
+			formula:  "median(x, y, z) * mean(4, 5, 6)",
+			expected: []string{"x", "y", "z"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			expr, err := parse(test.formula)
+			require.NoError(t, err)
+			require.Equal(t, test.expected, expr.getDependencies())
+		})
+	}
+}
+
+func TestBuildTransformations(t *testing.T) {
+	tests := []struct {
+		name            string
+		transformations []types.DataProviderTransformationConfig
+		knownSources    map[types.ValueId]interface{}
+		formula         string
+		expectedSize    int
+		expectedIndices []struct {
+			Index int
+			Id    types.ValueId
+		}
+	}{
+		{
+			name: "simple addition",
+			transformations: []types.DataProviderTransformationConfig{
+				{
+					Id:      "vtest1",
+					Formula: "s.test1 + 2",
+				},
+			},
+			knownSources: map[types.ValueId]interface{}{
+				"test1": nil,
+			},
+			expectedSize: 1,
+			expectedIndices: []struct {
+				Index int
+				Id    types.ValueId
+			}{
+				{Index: 0, Id: "vtest1"},
+			},
+		},
+		{
+			name: "multiple transformations",
+			transformations: []types.DataProviderTransformationConfig{
+				{
+					Id:      "vtest1",
+					Formula: "s.test1 + 2",
+				},
+				{
+					Id:      "vtest2",
+					Formula: "s.test2 + 2",
+				},
+				{
+					Id:      "vtest3",
+					Formula: "median(t.vtest1, t.vtest2)",
+				},
+			},
+			knownSources: map[types.ValueId]interface{}{
+				"test1": nil,
+				"test2": nil,
+			},
+			expectedSize: 3,
+			expectedIndices: []struct {
+				Index int
+				Id    types.ValueId
+			}{
+				{Index: 2, Id: "vtest3"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			transformations, err := BuildTransformations(test.transformations, test.knownSources)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedSize, len(transformations))
+			for _, expected := range test.expectedIndices {
+				require.Equal(t, expected.Id, transformations[expected.Index].Id)
+			}
+		})
+	}
+	// 	transformations, err := BuildTransformations([]types.DataProviderTransformationConfig{
+	// 	{
+	// 		Id:      "vtest1",
+	// 		Formula: "s.test1 + 2",
+	// 	},
+	// 	{
+	// 		Id:      "vtest2",
+	// 		Formula: "s.test2 + 2",
+	// 	},
+	// 	{
+	// 		Id:      "vtest3",
+	// 		Formula: "median(t.vtest1, t.vtest2)",
+	// 	},
+	// }, map[types.ValueId]interface{}{
+	// 	"test1": nil,
+	// 	"test2": nil,
+	// })
+	// require.NoError(t, err)
+	// require.Equal(t, 3, len(transformations))
+}
