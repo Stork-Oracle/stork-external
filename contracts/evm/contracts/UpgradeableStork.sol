d484e11d31a51a581dee908df82ac789b698a062
akawalsky <aaron@stork.network>
2025-04-18 13:03:53 -0400

    lock upgradeablestork at 0.8.24

--- contracts/evm/contracts/UpgradeableStork.sol
+++ contracts/evm/contracts/UpgradeableStork.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.24 <0.9.0;
+pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
