1eb75dfab3f133b0eba4be15e3a7e0491e39de1e
alex <alexander@stork.network>
2025-04-30 10:42:14 -0400

    interactor mock

--- apps/lib/chain_pusher/chain-interacter.go
+++ apps/lib/chain_pusher/chain-interacter.go
@@ -7,3 +7,28 @@ type ContractInteractor interface {
 	PullValues(encodedAssetIds []InternalEncodedAssetId) (map[InternalEncodedAssetId]InternalStorkStructsTemporalNumericValue, error)
 	BatchPushToContract(priceUpdates map[InternalEncodedAssetId]AggregatedSignedPrice) error
 }
+
+type MockContractInteractor struct {
+	ContractInteractor
+}
+
+func (m *MockContractInteractor) ListenContractEvents(
+	ctx context.Context,
+	ch chan map[InternalEncodedAssetId]InternalStorkStructsTemporalNumericValue,
+) {
+	// Do nothing
+}
+
+func (m *MockContractInteractor) PullValues(
+	encodedAssetIds []InternalEncodedAssetId,
+) (map[InternalEncodedAssetId]InternalStorkStructsTemporalNumericValue, error) {
+	// Do nothing
+	return nil, nil
+}
+
+func (m *MockContractInteractor) BatchPushToContract(
+	priceUpdates map[InternalEncodedAssetId]AggregatedSignedPrice,
+) error {
+	// Do nothing
+	return nil
+}
