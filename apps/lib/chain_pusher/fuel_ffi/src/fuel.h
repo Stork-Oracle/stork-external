fd8edfa720fbfa77942c20b0b05ac3046931379a
Harry Rackmil <harryrackmil@Harrys-MacBook-Pro.local>
2025-07-22 15:31:48 -0400

    add ffi

---
+++ apps/lib/chain_pusher/fuel_ffi/src/fuel.h
@@ -0,0 +1,28 @@
+#ifndef FUEL_H
+#define FUEL_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct FuelClient FuelClient;
+
+// Client management
+FuelClient* fuel_client_new(const char* config_json);
+void fuel_client_free(FuelClient* client);
+
+// Contract interactions
+char* fuel_get_latest_value(FuelClient* client, const uint8_t* id);
+char* fuel_update_values(FuelClient* client, const char* inputs_json);
+uint64_t fuel_get_wallet_balance(FuelClient* client);
+
+// Memory management
+void fuel_free_string(char* s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FUEL_H
