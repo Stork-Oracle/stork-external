788e6d5c9583961c7dad38c62f308416091c6565
Henry Baldwin <Henrymbaldwin@gmail.com>
2025-08-25 11:09:14 -0400

    Go linting configuration (#186)
    * Initial go linting setup.
    
    * Remove comprehensive lint command for now.
    
    * Comment explaining lack of check-go-formatting.
    
    * Comment phrasing.
    
    * Base on internal .golangci.yml

--- mk/lint.mk
+++ mk/lint.mk
@@ -1,8 +1,9 @@
-.PHONY: lint-links
 
+.PHONY: lint-links
 ### Checks for broken links in all markdown files
 lint-links:
 	@if [ -z "$(shell git diff-index --quiet HEAD --)" ]; then \
 		echo "Warning: you have uncommitted changes, this may return false positives for externalized links to this repo."; \
 	fi
 	@CURRENT_BRANCH=$(shell git rev-parse HEAD) linkspector check -c .linkspector.yml 
+
