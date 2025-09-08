caddd925ccc3da71a237b62c82c35aa27a6f6e6f
Henry Baldwin <henry@stork.network>
2025-09-08 15:06:33 -0400

    Fix links in doc comments and formatting.

--- chains/solana/sdks/stork-solana-sdk/src/lib.rs
+++ chains/solana/sdks/stork-solana-sdk/src/lib.rs
@@ -1,4 +1,3 @@
-
 /*! [![stork logo](https://raw.githubusercontent.com/Stork-Oracle/stork-external/refs/heads/main/public/stork-logo-slim.png)](https://stork.network)
 
 This is a Rust SDK to build Solana programs that consume Stork price feeds. This crate is maintained by [Stork Labs](https://stork.network).
@@ -7,11 +6,11 @@ A top level overview, as well as a small code sample, can be found in [the libra
 
 # Pull Model
 
-The Stork Solana SDK allows users to consume Stork price updates on a pull basis. This puts the responsibility of submitting the price updates on-chain to the user whenever they want to interact with an app that consumes Stork price feeds. Stork Labs maintains a [Chain Pusher](https://github.com/stork-oracle/stork-external/tree/main/apps/docs/chain_pusher.md) in order to do this.
+The Stork Solana SDK allows users to consume Stork price updates on a pull basis. This puts the responsibility of submitting the price updates on-chain to the user whenever they want to interact with an app that consumes Stork price feeds. Stork Labs maintains a [Chain Pusher](https://github.com/stork-oracle/stork-external/tree/main/apps/chain_pusher/README.md) in order to do this.
 
 # Stork Price Feed Accounts
 
-On Solana, Stork price feeds exist as on-chain accounts. These accounts are instances of the [TemporalNumericValueFeed](temporal_numeric_value::TemporalNumericValueFeed) account struct, and are created and owned by the Stork Oracle contract. These account have an ID which associates them with a specific asset, and a `latest_value` field which stores the latest price update. The ID of a TemporalNumericValueFeed account is determinined by taking the keccak256 hash of the asset ID. The `latest_value` field is an instance of the [TemporalNumericValue](temporal_numeric_value::TemporalNumericValue) struct, which holds the quantized price, and a unix timestamp indicating when the price was created.
+On Solana, Stork price feeds exist as on-chain accounts. These accounts are instances of the [TemporalNumericValueFeed](temporal_numeric_value::TemporalNumericValueFeed) account struct, and are created and owned by the Stork Oracle contract. These account have an ID which associates them with a specific asset, and a `latest_value` field which stores the latest price update. The ID of a TemporalNumericValueFeed account is determined by taking the keccak256 hash of the asset ID. The `latest_value` field is an instance of the [TemporalNumericValue](temporal_numeric_value::TemporalNumericValue) struct, which holds the quantized price, and a unix timestamp indicating when the price was created.
 
 # Anchor
 
@@ -22,8 +21,8 @@ use anchor_lang::{declare_id, prelude::Pubkey, pubkey};
 
 pub mod error;
 pub mod pda;
-pub mod temporal_numeric_value;
 pub mod program;
+pub mod temporal_numeric_value;
 
 /// The ID of the Stork Oracle program.
 pub const PROGRAM_ID: Pubkey = pubkey!("stork1JUZMKYgjNagHiK2KdMmb42iTnYe9bYUCDUk8n");
