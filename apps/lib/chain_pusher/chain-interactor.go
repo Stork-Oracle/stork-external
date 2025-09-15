872cfe23e395f6ff860b75109cce8ace30dee78b
Harry Rackmil <harryrackmil@Harrys-MacBook-Pro.local>
2025-09-15 12:01:17 -0400

    save progress

---
+++ apps/lib/chain_pusher/chain-interactor.go
@@ -0,0 +1,41 @@
+package chain_pusher
+
+import (
+	"context"
+)
+
+type ContractInteractor interface {
+	ListenContractEvents(ctx context.Context, ch chan map[InternalEncodedAssetId]InternalTemporalNumericValue)
+	PullValues(encodedAssetIds []InternalEncodedAssetId) (map[InternalEncodedAssetId]InternalTemporalNumericValue, error)
+	BatchPushToContract(priceUpdates map[InternalEncodedAssetId]AggregatedSignedPrice) error
+	GetWalletBalance() (float64, error)
+	Reconnect()
+}
+
+type MockContractInteractor struct {
+}
+
+func (m *MockContractInteractor) ListenContractEvents(
+	ctx context.Context,
+	ch chan map[InternalEncodedAssetId]InternalTemporalNumericValue,
+) {
+	// Do nothing
+}
+
+func (m *MockContractInteractor) PullValues(
+	encodedAssetIds []InternalEncodedAssetId,
+) (map[InternalEncodedAssetId]InternalTemporalNumericValue, error) {
+	// Do nothing
+	return nil, nil
+}
+
+func (m *MockContractInteractor) BatchPushToContract(
+	priceUpdates map[InternalEncodedAssetId]AggregatedSignedPrice,
+) error {
+	// Do nothing
+	return nil
+}
+
+func (m *MockContractInteractor) GetWalletBalance() (float64, error) {
+	return 0, nil
+}
