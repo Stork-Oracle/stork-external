aceaee16c9059bd3147caaab6dafe79d519b7980
akawalsky <aaron@stork.network>
2025-10-10 12:13:13 -0400

    add hip3_pusher boilerplate

---
+++ apps/hip3_pusher/src/hip3_pusher/__main__.py
@@ -0,0 +1,4 @@
+from hip3_pusher.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ apps/hip3_pusher/src/hip3_pusher/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello from HIP3 Pusher!")
+
+if __name__ == "__main__":
+    main()
