1ad977f85590aaf5a90d2895bf851e77157d9d7c
akawalsky <aaron@stork.network>
2025-12-17 16:56:06 -0500

    move gasLimit

--- chains/evm/contracts/stork/tasks/interact.ts
+++ chains/evm/contracts/stork/tasks/interact.ts
@@ -86,9 +86,7 @@ interactScope
         await contract.updateTemporalNumericValuesV1.send(updates, {
             value: Object.keys(customData).length === 0 ? updates.length : 0,
             customData,
-            txOptions: {
-                gasLimit: 10000000,
-            },
+            gasLimit: 10000000,
         });
     });
 
