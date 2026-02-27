beea6b26c1a687739afaf71181ff6eeb0d660ae8
Harry Rackmil <harryrackmil@Harrys-MacBook-Pro.local>
2026-02-27 17:24:19 -0500

    handle removing closed connections

--- apps/publisher_agent/pkg/runner.go
+++ apps/publisher_agent/pkg/runner.go
@@ -112,15 +112,17 @@ func (r *PublisherAgentRunner[T]) UpdateBrokerConnections() {
 		r.assetsByBroker[brokerUrl] = newAssetIDMap
 	}
 	r.outgoingConnectionsLock.RUnlock()
-	
+
 	// remove undesired connections
 	for url := range r.assetsByBroker {
 		_, exists := newBrokerMap[url]
 		if !exists {
 			r.outgoingConnectionsLock.RLock()
-			outgoingConnection := r.outgoingConnectionsByBroker[url]
+			outgoingConnection, outgoingConnectionExists := r.outgoingConnectionsByBroker[url]
 			r.outgoingConnectionsLock.RUnlock()
-			outgoingConnection.Remove()
+			if outgoingConnectionExists {
+				outgoingConnection.Remove()
+			}
 			delete(r.assetsByBroker, url)
 		}
 	}
